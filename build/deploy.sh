8edb80b4be09876e8ea8db450da8f2a3e4fea5ae
ATQQ <2604395430@qq.com>
2021-02-04 18:18:34 +0800

    chore: 更新CI/CD的配置文件

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,12 +7,12 @@ targetDir="/www/wwwroot/ep.sugarat.top/server"     # 目标目录
 echo "开始-----归档解压"
 tar -zvcf ${compressFile} ${compressDir}
 echo "开始-----拷贝至服务器"
-scp ${compressFile} ${user}@${origin}:./
-echo "开始-----部署"
-ssh -p22 ${user}@${origin} "rm -rf ${targetDir}/* && tar -zvxf ${compressFile} -C ${targetDir}"
-echo "开始-----安装依赖"
-ssh -p22 ${user}@${origin} "cd ${targetDir} && yarn install"
-echo "开始-----重新启动"
-ssh -p22 ${user}@${origin} "cd ${targetDir}/../ && cp ecosystem.config.js ${targetDir}/ && cd ${targetDir} && pm2 restart ecosystem.config.js --env production"
-echo "清理-----临时的文件"
-rm -rf $compressFile
+# scp ${compressFile} ${user}@${origin}:./
+# echo "开始-----部署"
+# ssh -p22 ${user}@${origin} "rm -rf ${targetDir}/* && tar -zvxf ${compressFile} -C ${targetDir}"
+# echo "开始-----安装依赖"
+# ssh -p22 ${user}@${origin} "cd ${targetDir} && yarn install"
+# echo "开始-----重新启动"
+# ssh -p22 ${user}@${origin} "cd ${targetDir}/../ && cp ecosystem.config.js ${targetDir}/ && cd ${targetDir} && pm2 restart ecosystem.config.js --env production"
+# echo "清理-----临时的文件"
+# rm -rf $compressFile
